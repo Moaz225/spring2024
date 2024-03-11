e96fbb51d7cfc0a329e0a1d3105c069f01c067c3
Moaz225 <130934796+Moaz225@users.noreply.github.com>
2024-03-11 15:21:51 +0500

    Create Review.java

---
+++ User.management/Review.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.domain;
+
+public class Review {
+    private Long id;
+    private User user;
+    private String comment;
+    private int rating;
+    // геттеры и сеттеры
+}
