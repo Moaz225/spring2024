454e90da56ce5fe918e1226ee41d2a3c373209af
Moaz225 <130934796+Moaz225@users.noreply.github.com>
2024-03-11 15:20:46 +0500

    Create User.java

---
+++ User.management/User.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+    // геттеры и сеттеры
+}
